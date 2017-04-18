20733c39f09c14a6cf931c4664ba6ae2f9ba1542
Curtis Rueden <ctrueden@wisc.edu>
2017-04-18 08:51:40 -0500

    arxiv.sh: remove natwidth and natheight
    Without them, the manuscript does not build locally:
    
        ! LaTeX Error: Cannot determine size of graphic in figure-1/figure-config1-labe
        l.png (no BoundingBox).
    
    To fix it, I added natwidth and natheight as described here:
    
        https://tex.stackexchange.com/a/17737
    
    But with them in place, arXiv's processor dies with:
    
        ! Undefined control sequence.
        <argument> ...@base \Gin@ext image\GPT@AttrShort
        \ifx \GPT@print \ltx@empty...
        l.977 ...t=713]{figure-1/figure-config1-label.png}
    
        ?
        ! Emergency stop.
        <argument> ...@base \Gin@ext image\GPT@AttrShort
        \ifx \GPT@print \ltx@empty...
        l.977 ...t=713]{figure-1/figure-config1-label.png}
    
    So my solution is to strip them out when bundling the arXiv archive.
    
    Thanks to Jim the arXiv admin for knowing how to fix this!

--- arxiv.sh
+++ arxiv.sh
@@ -2,6 +2,7 @@
 dir="arxiv"
 mkdir -p "$dir"
 cp bmcart.cls imagej2.tex imagej2.bbl imagej2.gls "$dir"
+sed -i '' -e 's/,natwidth=[0-9]*,natheight=[0-9]*//g' "$dir/imagej2.tex"
 for fig in $(grep 'includegraphics\[' imagej2.tex | sed 's/.*{\(.*\)}/\1/')
 do
   dest="$dir/$(dirname "$fig")"
